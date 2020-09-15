1cccc1d555df8045e92915021afe35e715da012d
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2020-09-15 12:36:39 -0300

    home completa

---
+++ componentes/navbar.php
@@ -0,0 +1,45 @@
+<html>
+
+<head>
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <div class="container">
+            <a class="navbar-brand cinzel-decorative text-secondary" href="index.html">
+                <img src="images/logo-original.png" width="50" height="30" class="d-inline-block align-top" alt="">
+                Exclusiva Isenções
+            </a>
+            <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+                <span class="navbar-toggler-icon text-secondary"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse roboto-condensed" id="conteudoNavbarSuportado">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Serviços
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Ação</a>
+                        <a class="dropdown-item" href="#">Outra ação</a>
+                        <a class="dropdown-item" href="#">Algo mais aqui</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Veículos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Quem&nbspsomos</a>
+                </li>
+                
+            </ul>
+        </div>
+    </nav>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/style.min.css">
+    <title>Exclusiva Isenções</title>
+</head>
+
+<body class="">
+    <?php include 'componentes/navbar.php'; ?>
+    <div class="">
+        <img src="images/663894.jpg" class="img-fluid" alt="Imagem responsiva">
+        <!-- <div class="text-center">
+        <h1 class="cinzel-decorative text-home">Exclusiva Isenções</h1>
+    </div> -->
+    </div>
+    <div class="container cards-custom roboto-condensed">
+        <div class="card-deck">
+            <div class="card border-0 rounded text-center p-3">
+                <div class="text-center">
+                    <img class="card-img-top " src="images/reliable.png" alt="Imagem de capa do card" style="width: 80px;">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Confiabilidade</h5>
+                    <hr>
+                    <p class="card-text ">Acompanhe seu processo de perto e tire todas suas dúvidas.</p>
+                </div>
+            </div>
+            <div class="card border-0 rounded rounded-top text-center p-3">
+                <div class="text-center">
+                    <img class="card-img-top " src="images/team.png" alt="Imagem de capa do card" style="width: 80px;">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Assessoria Completa</h5>
+                    <hr>
+                    <p class="card-text">Oferecemos todos os serviços necessários para você obter seu carro 0 KM com as isenções necessárias.</p>
+                </div>
+            </div>
+            <div class="card border-0 rounded text-center p-3">
+                <div class="text-center">
+                    <img class="card-img-top " src="images/proposal.png" alt="Imagem de capa do card" style="width: 80px;">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Avaliação Gratuíta</h5>
+                    <hr>
+                    <p class="card-text ">Envie seu laudo médico ou exames e nós avaliamos gratuitamente.</p>
+                </div>
+            </div>
+            <div class="card border-0 rounded text-center p-3">
+                <div class="text-center">
+                    <img class="card-img-top " src="images/stamp.png" alt="Imagem de capa do card" style="width: 80px;">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Indice de Aprovação</h5>
+                    <hr>
+                    <p class="card-text">Com equipe altamente qualificada, temos um alto índice de aprovações dos processos.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="">
+        <img src="images/arte-1.png" class="img-fluid" alt="Imagem responsiva">
+    </div>
+    <div class="jumbotron jumbotron-fluid text-center roboto-condensed bg-nav text-white">
+        <div class="container">
+            <div>
+                <i class="fab fa-instagram mt-3" style="font-size: 25px;"></i>
+                <i class="fab fa-facebook-f ml-2 mt-3 ml-3" style="font-size: 25px;"></i>
+            </div>
+            <h6 class="">Exclusiva Isenções</h6>
+            <h6 class="">contato@exclusivaisencoes.com.br</h6>
+            <h6 class="">www.exclusivaisencoes.com.br</h6>
+        </div>
+    </div>
+    <div class="text-center">
+        <small class="text-black-50">© Todos os direitos reservados a Exclusiva Isenções</small>
+    </div>
+
+
+
+
+
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
